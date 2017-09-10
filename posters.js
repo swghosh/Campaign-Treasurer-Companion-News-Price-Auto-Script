d3de2f5c2700ccf2fe4a433ec312a8344b25067f
SwG Ghosh <codecrafts.cf@icloud.com>
2017-09-10 17:21:13 +0530

    Added comments to server.js, posters.js and use of constants for making full URL in posters.js

--- posters.js
+++ posters.js
@@ -1,21 +1,37 @@
+#!/bin/env node
+// Node.js single file Module to allow making price and news updates to Campaign Treasurer Companion
+
+// import the required modules
 var http = require('http');
 var url = require('url');
 var querystring = require('querystring');
 
+// constants to use with URL
+const hostname = 'campaigntreasurercompanionwebapp.ap-south-1.elasticbeanstalk.com';
+const usernamePassword = 'ctcadmin:christuniversity2017prayasctc';
+
+// paths to use URL(s) in HTTP requests
+const pathForNewsUpdate = '/masterpanel/addnews.php';
+const pathForPriceOf = '/masterpanel/getprice.php';
+const pathForPriceUpdate = '/masterpanel/updateprice.php';
+
+// function to post news content and run a callback
 exports.postNews = function(content, callback) {
 
+    // post data contains the news content
     var postData = querystring.stringify({
         content: content
     });
 
+    // descriptor to make a HTTP request
     var options = { 
-        href: 'http://ctcadmin:christuniversity2017prayasctc@campaigntreasurercompanionwebapp.ap-south-1.elasticbeanstalk.com/masterpanel/addnews.php',
-        origin: 'http://campaigntreasurercompanionwebapp.ap-south-1.elasticbeanstalk.com',
+        href: 'http://' + usernamePassword + '@' + hostname + pathForNewsUpdate,
+        origin: 'http://' + hostname,
         protocol: 'http:',
-        host: 'campaigntreasurercompanionwebapp.ap-south-1.elasticbeanstalk.com',
-        hostname: 'campaigntreasurercompanionwebapp.ap-south-1.elasticbeanstalk.com',
+        host: hostname,
+        hostname: hostname,
         port: '',
-        path: '/masterpanel/addnews.php',
+        path: pathForNewsUpdate,
         method: 'POST',
         headers: {
             'Authorization': 'Basic Y3RjYWRtaW46Y2hyaXN0dW5pdmVyc2l0eTIwMTdwcmF5YXNjdGM=',
@@ -24,6 +40,7 @@ exports.postNews = function(content, callback) {
         }
     };
 
+    // make the HTTP request to send news update
     var request = http.request(options, (response) => {
         var data = '';
         response.on('data', (chunk) => {
@@ -31,68 +48,77 @@ exports.postNews = function(content, callback) {
         });
         response.on('end', () => {
             console.log(data);
-
+            // perform callback
             callback(data);
         });
     });
 
+    // write the post data to the request and end the request
     request.write(postData);
     request.end();
 };
 
+// function to find price of an item and run a callback with it
 exports.priceOf = function(item, callback) {
 
-    var request = http.get('http://ctcadmin:christuniversity2017prayasctc@campaigntreasurercompanionwebapp.ap-south-1.elasticbeanstalk.com/masterpanel/getprice.php?name=' + item, (response) => {
+    // make the HTTP request to get price of item
+    var request = http.get('http://' + usernamePassword + '@' + hostname + pathForPriceOf + '?name=' + item, (response) => {
         var data = '';
         response.on('data', (chunk) => {
             data += chunk;
         });
         response.on('end', () => {
             console.log(data);
-
+            // perform callback with price
             var price = parseFloat(data.split("\n")[1]);
             callback(price);
         });
     });
     
+    // end the request
     request.end();
 };
 
+// function to post price update and run a callback
 exports.updatePrice = function(item, price, callback) {
     
-        var postData = querystring.stringify({
-            item: item,
-            current: price
+    // post data contains the item name and new price
+    var postData = querystring.stringify({
+        item: item,
+        current: price
+    });
+
+    // descriptor to make a HTTP request
+    var options = { 
+        href: 'http://' + usernamePassword + '@' + hostname + pathForPriceUpdate,
+        origin: 'http://' + hostname,
+        protocol: 'http:',
+        host: hostname,
+        hostname: hostname,
+        port: '',
+        path: pathForPriceUpdate,
+        method: 'POST',
+        headers: {
+            'Authorization': 'Basic Y3RjYWRtaW46Y2hyaXN0dW5pdmVyc2l0eTIwMTdwcmF5YXNjdGM=',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length' : postData.length
+        }
+    };
+
+    // make the HTTP request to send news update
+    var request = http.request(options, (response) => {
+        var data = '';
+        response.on('data', (chunk) => {
+            data += chunk;
         });
-    
-        var options = { 
-            href: 'http://ctcadmin:christuniversity2017prayasctc@campaigntreasurercompanionwebapp.ap-south-1.elasticbeanstalk.com/masterpanel/updateprice.php',
-            origin: 'http://campaigntreasurercompanionwebapp.ap-south-1.elasticbeanstalk.com',
-            protocol: 'http:',
-            host: 'campaigntreasurercompanionwebapp.ap-south-1.elasticbeanstalk.com',
-            hostname: 'campaigntreasurercompanionwebapp.ap-south-1.elasticbeanstalk.com',
-            port: '',
-            path: '/masterpanel/updateprice.php',
-            method: 'POST',
-            headers: {
-                'Authorization': 'Basic Y3RjYWRtaW46Y2hyaXN0dW5pdmVyc2l0eTIwMTdwcmF5YXNjdGM=',
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Content-Length' : postData.length
-            }
-        };
-    
-        var request = http.request(options, (response) => {
-            var data = '';
-            response.on('data', (chunk) => {
-                data += chunk;
-            });
-            response.on('end', () => {
-                console.log(data);
-    
-                callback(data);
-            });
+        response.on('end', () => {
+            console.log(data);
+            // perform callback
+            callback(data);
         });
-    
-        request.write(postData);
-        request.end();
+    });
+
+    // write the post data to the request and end the request
+    request.write(postData);
+    request.end();
 };
--- server.js
+++ server.js
@@ -1,109 +1,154 @@
+#!/bin/env node
+// Node.js Application to publish automatic time based news and price updates to Campain Treasurer Companion
+
+// import required modules
 var http = require('http');
 var fs = require('fs');
 
 var posters = require('./posters.js');
 
+// boolean switch to allow script control
 var scriptOneSwitch = false;
 var scriptTwoSwitch = false;
 
-var durationNewsAndPrice = 20000;
+// set the duration between a news update and a price update
+const durationNewsAndPrice = 20000;
 
+// read json files with news, price update content as a JSON object
 var scriptFileOne = JSON.parse(fs.readFileSync('/home/ubuntu/campaigntreasurercompanionnewsscript/file1.json', 'utf8'));
 var scriptFileTwo = JSON.parse(fs.readFileSync('/home/ubuntu/campaigntreasurercompanionnewsscript/file2.json', 'utf8'));
 
+// function to handle requests, process tasks and generate appropriate responses
 var handle = function(request, response) {
     console.log(request.url, "was requested");
+
+    // request to start auto update script part 1
     if(request.url == '/start1') {
+        // set first switch to true
         scriptOneSwitch = true;
+        // start from 0 index
         var index = 0;
+        // run a loop to post all auto updates segregated by time id (time id is in minutes)
         for(var i in scriptOne) {
+           // set timeout based on time id in minutes
            setTimeout(() => {
                if(scriptOneSwitch) {
+                // run updater to post update
                 updater(scriptOne[index].news, scriptOne[index].percentage, scriptOne[index++].item);
+                // set first switch to false on end
                 if(index == scriptOne.length) {
                     scriptOneSwitch = false;
                 }
                }
            }, scriptOne[i].timeId * 60 * 1000);
         }
+        // send appropriate success response as text
         response.writeHead(200, {'Content-Type': 'text/plain'});
         response.end('Script 1 started');
     }
+
+    // request to start auto update script part 2 
     else if(request.url == '/start2') {
+        // set second switch to true
         scriptTwoSwitch = true;
+        // start from 0 index
         var index = 0;
+        // run a loop to post all auto updates segregated by time id (time id is in minutes)
         for(var i in scriptTwo) {
+           // set timeout based on time id in minutes
            setTimeout(() => {
                 if(scriptTwoSwitch) {
+                    // run updater to post update
                     updater(scriptTwo[index].news, scriptTwo[index].percentage, scriptTwo[index++].item);
+                    // set second switch to false on end
                     if(index == scriptTwo.length) {
                         scriptTwoSwitch = false;
                     }
                 }
            }, scriptTwo[i].timeId * 60 * 1000);
         }
+        // send appropriate success response as text
         response.writeHead(200, {'Content-Type': 'text/plain'});
         response.end('Script 2 started');
     }
+
+    // request to stop auto update script part 1
     else if(request.url == '/stop') {
+        // set both switches to false
         scriptOneSwitch = scriptTwoSwitch = false;
+        // send appropriate success response as text
         response.writeHead(200, {'Content-Type': 'text/plain'});
         response.end('All scripts are off now!');
     }
+
+    // request to stop auto update script part 2
     else {
+        // send appropriate response with status as text
         response.writeHead(200, {'Content-Type': 'text/plain'});
         response.end('Host is up and doing!\n' + 'script one running: ' + scriptOneSwitch + '\nscript two running: ' + scriptTwoSwitch);
     }
+
 };
 
+// create the web server and make it listen to port 8080
 var server = http.createServer(handle).listen(8080);
 
+// initialise empty arrays for holding objects of auto update scripts
 var scriptOne = [];
+var scriptTwo = [];
 
+// process array of objects for auto update script part 1
 for(var index in scriptFileOne) {
     var script = scriptFileOne[index];
-
+    // make objects with news, time id, percentage, item
     scriptOne[index] = {};
     scriptOne[index].news = script[0];
     scriptOne[index].timeId = script[1];
     scriptOne[index].percentage = script[2];
     scriptOne[index].item = script[3];
 
+    // use negative for percentage strings with DOWN
     if(scriptOne[index].percentage.trim().endsWith('DOWN')) {
         scriptOne[index].percentage = - parseFloat(scriptOne[index].percentage);
     }
+    // otherwise use positive percentage
     else {
         scriptOne[index].percentage = parseFloat(scriptOne[index].percentage);
     }
-
-
 }
 
-var scriptTwo = [];
-
+// process array of objects for auto update script part 2
 for(var index in scriptFileTwo) {
     var script = scriptFileTwo[index];
-
+    // make objects with news, time id, percentage, item
     scriptTwo[index] = {};
     scriptTwo[index].news = script[0];
     scriptTwo[index].timeId = script[1];
     scriptTwo[index].percentage = script[2];
     scriptTwo[index].item = script[3];
 
+    // use negative for percentage strings with DOWN
     if(scriptTwo[index].percentage.trim().endsWith('DOWN')) {
         scriptTwo[index].percentage = - parseFloat(scriptTwo[index].percentage);
     }
+    // otherwise use positive percentage
     else {
         percentage = parseFloat(scriptTwo[index].percentage);
     }
 }
 
+// function that'll process a news update and price update with an interval
 var updater = function(news, percentage, item) {
+    // post the news update with the content
     posters.postNews(news, (output) => { });
+    // perform price update after a certain constant duration
     setTimeout(() => {
+        // check the price of the current item name and callback 
         posters.priceOf(item, (value) => {
+            // calculate the difference in price and new price
             var difference = (value * percentage) / 100;
             var newPrice = value + difference;
+            // post the price update with item name and new value
             posters.updatePrice(item, newPrice, (output) => { });
         });
     }, durationNewsAndPrice);
