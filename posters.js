1657db2c494e270cd9eb82860769aba9a27e4bdf
SwG Ghosh <codecrafts.cf@icloud.com>
2018-07-27 02:26:13 +0530

    Refactored posters.js

--- posters.js
+++ posters.js
@@ -3,17 +3,27 @@
 
 // import the required modules
 var http = require('http');
-var url = require('url');
 var querystring = require('querystring');
 
 // constants to use with URL
-const hostname = 'campaigntreasurercompanionwebapp.ap-south-1.elasticbeanstalk.com';
-const usernamePassword = 'ctcadmin:christuniversity2017prayasctc';
+const hostname = 'localhost';
+const authorisationToken = process.env.CTCADMINTOKEN || 'someuser:somepassword';
 
 // paths to use URL(s) in HTTP requests
 const pathForNewsUpdate = '/masterpanel/addnews.php';
 const pathForPriceOf = '/masterpanel/getprice.php';
 const pathForPriceUpdate = '/masterpanel/updateprice.php';
+
+var descriptor = {
+    hostname: hostname,
+    // path: somePath,
+    // method: 'POST' || 'GET',
+    auth: authorisationToken,
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length' : 0
+    }
+}
 
 // function to post news content and run a callback
 exports.postNews = function(content, callback) {
@@ -24,21 +34,10 @@ exports.postNews = function(content, callback) {
     });
 
     // descriptor to make a HTTP request
-    var options = { 
-        href: 'http://' + usernamePassword + '@' + hostname + pathForNewsUpdate,
-        origin: 'http://' + hostname,
-        protocol: 'http:',
-        host: hostname,
-        hostname: hostname,
-        port: '',
-        path: pathForNewsUpdate,
-        method: 'POST',
-        headers: {
-            'Authorization': 'Basic Y3RjYWRtaW46Y2hyaXN0dW5pdmVyc2l0eTIwMTdwcmF5YXNjdGM=',
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Content-Length' : postData.length
-        }
-    };
+    var options = descriptor;
+    options.path = pathForNewsUpdate
+    options.method = 'POST'
+    options.headers['Content-Length'] = postData.length
 
     // make the HTTP request to send news update
     var request = http.request(options, (response) => {
@@ -47,7 +46,6 @@ exports.postNews = function(content, callback) {
             data += chunk;
         });
         response.on('end', () => {
-            console.log(data);
             // perform callback
             callback(data);
         });
@@ -61,16 +59,21 @@ exports.postNews = function(content, callback) {
 // function to find price of an item and run a callback with it
 exports.priceOf = function(item, callback) {
 
+    // descriptor to make a HTTP request
+    var options = descriptor;
+    options.path = pathForPriceOf + '?' + querystring.stringify({name: item})
+    options.method = 'GET'
+    options.headers = {}
+
     // make the HTTP request to get price of item
-    var request = http.get('http://' + usernamePassword + '@' + hostname + pathForPriceOf + '?name=' + item, (response) => {
+    var request = http.request(options, (response) => {
         var data = '';
         response.on('data', (chunk) => {
             data += chunk;
         });
         response.on('end', () => {
-            console.log(data);
             // perform callback with price
-            var price = parseFloat(data.split("\n")[1]);
+            var price = parseFloat(data.split('\n')[1]);
             callback(price);
         });
     });
@@ -89,21 +92,10 @@ exports.updatePrice = function(item, price, callback) {
     });
 
     // descriptor to make a HTTP request
-    var options = { 
-        href: 'http://' + usernamePassword + '@' + hostname + pathForPriceUpdate,
-        origin: 'http://' + hostname,
-        protocol: 'http:',
-        host: hostname,
-        hostname: hostname,
-        port: '',
-        path: pathForPriceUpdate,
-        method: 'POST',
-        headers: {
-            'Authorization': 'Basic Y3RjYWRtaW46Y2hyaXN0dW5pdmVyc2l0eTIwMTdwcmF5YXNjdGM=',
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Content-Length' : postData.length
-        }
-    };
+    var options = descriptor;
+    options.path = pathForPriceUpdate
+    options.method = 'POST'
+    options.headers['Content-Length'] = postData.length
 
     // make the HTTP request to send news update
     var request = http.request(options, (response) => {
@@ -112,7 +104,6 @@ exports.updatePrice = function(item, price, callback) {
             data += chunk;
         });
         response.on('end', () => {
-            console.log(data);
             // perform callback
             callback(data);
         });
